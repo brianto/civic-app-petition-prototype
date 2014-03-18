87f2529ae17548341e43f9393a991e6573388bd1
Brian To <bxt5647@rit.edu>
2014-03-17 22:24:23 -0400

    Login works, but is shitty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  # Y U NO WORK?
+  # filter_parameter_logging :password
+  
   before_action do
     @constants = Constants.first
+  end
+  
+  helper_method :current_user
+
+  private
+  
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+  
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
   end
 end
