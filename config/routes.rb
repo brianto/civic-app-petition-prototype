9606f8619e120efb1fd2f382af0de7444c190baf
Andres Ruiz <andresruiz2010@gmail.com>
2014-04-06 13:52:12 -0400

    Add active admin into the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CivicAppPetitionPrototype::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'petitions#index'
   
   resources :petitions do
