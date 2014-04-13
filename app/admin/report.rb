93eefcea1b85efc15fe5a2f486e71fd6aaf31b13
Andres Ruiz <andresruiz2010@gmail.com>
2014-04-13 13:17:04 -0400

    Fix reporting from admin panel

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -14,5 +14,21 @@ ActiveAdmin.register Report do
   #  permitted
   # end
   menu :parent => "Petitions"
+
+  controller do
+    def create
+
+      petition = Petition.find_by_id(params[:report][:petition_id])
+      politician = Resident.find_by_id(params[:response][:resident_id])
+      statement = params[:response][:statement]
+
+      @response = Response.new(:petition => petition, :politician => politician, :statement => statement)
+      if @response
+        @response.save!
+      end
+
+      redirect_to admin_responses_url
+    end
+  end
   
 end
